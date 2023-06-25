3333287cd38d27faf1e76ee362ff53a43539d92c
irene8941 <irene8941@naver.com>
2023-06-25 20:04:00 +0900

    [Tmp]

--- Part.3/2. 12줄의 코드로 시작하기/1.프로젝트 세팅/app.js
+++ Part.3/2. 12줄의 코드로 시작하기/1.프로젝트 세팅/app.js
@@ -1,8 +1,14 @@
+// 페이징 9분~
+
 const container = document.getElementById("root");
 const ajax = new XMLHttpRequest();
 const content = document.createElement("div");
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 const CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+// 공유자원
+const store = {
+    currentPage: 1,
+};
 
 // JSON 형태의 응답 값(ajax.response)을 객체로 변환
 function getData(url) {
@@ -18,12 +24,12 @@ function newsFeed() {
 
     newsList.push('<ul>')
     
-    for (let i = 0; i < 10; i++) {
+    for (let i = store.currentPage - 1; i < 10; i++) {
         const div = document.createElement("div");
     
         newsList.push(`
         <li>
-            <a href="#${newsFeed[i]
+            <a href="#/show/${newsFeed[i]
             .id}"> ${newsFeed[i]
             .title} (${newsFeed[i]
             .comments_count})
@@ -32,8 +38,13 @@ function newsFeed() {
         `);
     }
 
-
     newsList.push('</ul>');
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage - 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `);
 
     container.innerHTML = newsList.join('');
 }
@@ -61,6 +72,9 @@ function router() {
     const routePath = location.hash;
 
     if (routePath === '') {
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0){
+        store.currentPage += 2;
         newsFeed();
     } else {
         newsDetail();
