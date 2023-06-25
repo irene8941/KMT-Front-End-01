35fb21671f4524ae613eff3b5ac907841463f10f
irene8941 <irene8941@naver.com>
2023-06-25 14:48:21 +0900

    [Add] ajax 중복 코드 개선

--- Part.3/2. 12줄의 코드로 시작하기/1.프로젝트 세팅/app.js
+++ Part.3/2. 12줄의 코드로 시작하기/1.프로젝트 세팅/app.js
@@ -4,20 +4,21 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);
-ajax.send();
-
 // JSON 형태의 응답 값(ajax.response)을 객체로 변환
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+    ajax.open('GET', url, false);
+    ajax.send();
+
+    return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function() {
     const id = location.hash.substr(1);
 
-    ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-    ajax.send();
-
-    const newsContent = JSON.parse(ajax.response);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
     const title = document.createElement('h1');
 
     title.innerHTML = newsContent.title;
